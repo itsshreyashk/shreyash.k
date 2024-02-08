adec03378e1e07beafc7b292b4211c7b2b4c4d1f
itsshreyashk <shreyashkumar.me@gmail.com>
2024-02-08 14:14:32 +0530

    done

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Nav from '../utils/nav';
 import { PlaceOrderButton, Impress, TrackOrder } from '../utils/elements';
 const Order: React.FC = () => {
-
+    document.title = "Order Now";
     return (<>
         <TrackOrder />
         <Impress />
