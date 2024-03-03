dab9e0094b1a55f218c0214bae6d89ab34e3b880
itsshreyashk <shreyashkumar.me@gmail.com>
2024-03-03 11:14:47 +0530

    v3

--- src/utils/nav.tsx
+++ src/utils/nav.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Nav: React.FC = () => {
     return (
         <>
+        
             <div className="fixed top-0 w-full flex justify-center p-1 ">
                 <div className=" w-full max-w-[800px] flex justify-center rounded-xl bg-[transparent] backdrop-blur-xl px-4 py-2 space-x-8">
                     <Link to={'/'} className='text-white hover:scale-[2] transform transition duration-200'>
